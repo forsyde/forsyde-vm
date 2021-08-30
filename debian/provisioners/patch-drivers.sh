512476391aa25c52fdd294c73d5eddf8fb29209f
jordao <jordao@kth.se>
2021-08-30 10:38:31 +0200

    Fixed the udev rules permissions

--- debian/provisioners/patch-drivers.sh
+++ debian/provisioners/patch-drivers.sh
@@ -3,16 +3,21 @@
 ###begin documentation##########################################################
 #
 # Author:  George Ungureanu <ugeorge@kth.se>
-# Date:    2014.12.19
+#          Rodolfo Jordao <jordao@kth.se>
+# Date:    2021.08.30
 # Purpose: fix the correct USB Blaster device driver rules
 #
 ###end documentation############################################################
 mkdir -p /etc/udev/rules.d
 touch /etc/udev/rules.d/91-altera-usb-blaster.rules
 
-echo 'SUBSYSTEM=="usb", ATTR{idVendor}=="09fb", ATTR{idProduct}=="6001", MODE="0666"\n' >> /etc/udev/rules.d/91-altera-usb-blaster.rules
-echo 'SUBSYSTEM=="usb", ATTR{idVendor}=="09fb", ATTR{idProduct}=="6002", MODE="0666"\n' >> /etc/udev/rules.d/91-altera-usb-blaster.rules
-echo 'SUBSYSTEM=="usb", ATTR{idVendor}=="09fb", ATTR{idProduct}=="6003", MODE="0666"\n' >> /etc/udev/rules.d/91-altera-usb-blaster.rules
-echo 'SUBSYSTEM=="usb", ATTR{idVendor}=="09fb", ATTR{idProduct}=="6010", MODE="0666"\n' >> /etc/udev/rules.d/91-altera-usb-blaster.rules
+# Intel FPGA Download Cable
+echo 'SUBSYSTEM=="usb", ATTR{idVendor}=="09fb", ATTR{idProduct}=="6001", MODE="0666"' >> /etc/udev/rules.d/91-altera-usb-blaster.rules
+echo 'SSUBSYSTEM=="usb", ATTR{idVendor}=="09fb", ATTR{idProduct}=="6002", MODE="0666"'>> /etc/udev/rules.d/91-altera-usb-blaster.rules
+echo 'SSUBSYSTEM=="usb", ATTR{idVendor}=="09fb", ATTR{idProduct}=="6003", MODE="0666"'>> /etc/udev/rules.d/91-altera-usb-blaster.rules
+
+# Intel FPGA Download Cable II
+echo 'SUBSYSTEM=="usb", ATTR{idVendor}=="09fb", ATTR{idProduct}=="6010", MODE="0666"' >> /etc/udev/rules.d/91-altera-usb-blaster.rules
+echo 'SUBSYSTEM=="usb", ATTR{idVendor}=="09fb", ATTR{idProduct}=="6810", MODE="0666"' >> /etc/udev/rules.d/91-altera-usb-blaster.rules
 
 udevadm control --reload
